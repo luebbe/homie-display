d17f1eba17bc602432499618c89eb1b991abc222
Lübbe <luebbe.opensource@gmail.com>
2019-08-19 20:37:57 +0200

    Improve formatting for current weather frame

--- src/CurrentWeatherFrame.cpp
+++ src/CurrentWeatherFrame.cpp
@@ -16,19 +16,18 @@ void CurrentWeatherFrame::drawFrame(
     OLEDDisplayUiState &state,
     int16_t x, int16_t y)
 {
+  // Icon font width = 42 pixels
+  display.setFont(Meteocons_Plain_42);
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.drawString(24 + x, 5 + y, data.iconMeteoCon);
+
   display.setFont(ArialMT_Plain_10);
   display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.drawString(58 + x, 5 + y, data.description);
+  display.drawString(48 + x, 5 + y, data.description);
 
   display.setFont(ArialMT_Plain_24);
   String temp = String(data.temp, 1) + "°C";
-  display.drawString(58 + x, 15 + y, temp);
-
-  display.setFont(Meteocons_Plain_42);
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
-  String weatherIcon = data.iconMeteoCon;
-  int weatherIconWidth = display.getStringWidth(weatherIcon);
-  display.drawString(30 + x - weatherIconWidth / 2, 5 + y, weatherIcon);
+  display.drawString(48 + x, 15 + y, temp);
 }
 
 void CurrentWeatherFrame::update(String apiKey, String locationId, String language, boolean isMetric)
