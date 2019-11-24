aaa29fa46aa0839577fa09969dc789d27c9fadea
Lübbe <luebbe.opensource@gmail.com>
2019-11-24 22:21:23 +0100

    re-enabled basic display

--- src/homie-display.cpp
+++ src/homie-display.cpp
@@ -11,25 +11,22 @@
 #include "welcome.hpp"
 #include "MqttCollector.hpp"
 
-OtaDisplay ota(NULL);
-Welcome welcome(FW_NAME, FW_VERSION);
-
 MqttCollector mqttCollector("MqttFrame", 0);
 
 // Display & UI
-// #include <SSD1306.h>
+#include <SSD1306.h>
 // #include <OLEDDisplayUi.h>
 
-// const int I2C_DISPLAY_ADDRESS = 0x3c;
-// const int PIN_SDA = 4; // =D2 on Wemos
-// const int PIN_SCL = 5; // =D1 on Wemos
+const int I2C_DISPLAY_ADDRESS = 0x3c;
+const int PIN_SDA = 4; // =D2 on Wemos
+const int PIN_SCL = 5; // =D1 on Wemos
 
 // Connected peripherals
-// SSD1306Wire display(I2C_DISPLAY_ADDRESS, PIN_SDA, PIN_SCL);
+SSD1306Wire display(I2C_DISPLAY_ADDRESS, PIN_SDA, PIN_SCL);
 // OLEDDisplayUi ui(&display);
 
-// OtaDisplaySSD1306 ota(display, NULL);
-// WelcomeSSD1306 welcome(display, FW_NAME, FW_VERSION);
+OtaDisplaySSD1306 ota(display, NULL);
+WelcomeSSD1306 welcome(display, FW_NAME, FW_VERSION);
 
 // void resumeTransition()
 // {
@@ -99,10 +96,10 @@ void setup()
   mqttCollector.beforeSetup();
 
   // Initializes I2C for BME280 sensor and display
-  // Homie.getLogger() << "• Wire begin SDA=" << PIN_SDA << " SCL=" << PIN_SCL << endl;
-  // Wire.begin(PIN_SDA, PIN_SCL);
+  Homie.getLogger() << "• Wire begin SDA=" << PIN_SDA << " SCL=" << PIN_SCL << endl;
+  Wire.begin(PIN_SDA, PIN_SCL);
 
-  // // Display and UI
+  // Display and UI
   // ui.setTargetFPS(30);
   // ui.setFrameAnimation(SLIDE_LEFT);
   // ui.setTimePerFrame(5000);
@@ -110,9 +107,9 @@ void setup()
   // ui.disableAutoTransition();
   // ui.disableAllIndicators();
   // ui.init();
-  // display.flipScreenVertically();
-  // display.setColor(WHITE);
-  // display.display();
+  display.flipScreenVertically();
+  display.setColor(WHITE);
+  display.display();
 
   // Homie.onEvent(onHomieEvent);
 
